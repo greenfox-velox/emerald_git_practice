307c303f1a8b4a1bce350bce9264f2c9c3d4075a
zsolt-fekete <zsoltfekete.zsf@gmail.com>
2016-08-02 09:40:02 +0200

    C1

---
+++ Cheer.java
@@ -0,0 +1,9 @@
+public class Cheer {
+
+  public static void cheerer(String name){
+    System.out.println("Cheer for: " + name + " ! ");
+
+  public static void main(String[] args) {
+      cheerer("Emerald");
+  }
+}
