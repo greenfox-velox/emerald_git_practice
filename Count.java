dc3380fbef762378e3bbf018286e5c1d1cc989b3
zsolt-fekete <zsoltfekete.zsf@gmail.com>
2016-08-02 10:50:46 +0200

    C7

--- Count.java
+++ Count.java
@@ -1,7 +1,7 @@
 public class Count {
 
-  public String ageCount(String age,String countless) {
-        System.out.println(age + " month: 7 " +countless);
+  public String ageCount(String age,String count) {
+        System.out.println(age + " month: 7 " +count);
 
   public static void main(String[] args) {
       ageCount("30");
