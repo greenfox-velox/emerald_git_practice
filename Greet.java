759c614f491420e0b9220851de29a39b477c7b00
Peter Lodri <silentpocok@gmail.com>
2016-08-02 09:41:46 +0200

    a2, added parameter to greeter

--- Greet.java
+++ Greet.java
@@ -1,5 +1,10 @@
 class Greet {
+
+   public void greeter(String name){
+     System.out.println(name);
+   }
+
    public static void main(String[] args){
-      System.out.println("Pocok");
+      greeter("Pocok"),
    }
 }
